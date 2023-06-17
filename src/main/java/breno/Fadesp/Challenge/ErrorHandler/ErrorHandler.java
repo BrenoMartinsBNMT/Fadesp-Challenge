273f25d4f943048a1e45d9c339eacd35677e8da2
Breno Martins <brenomartinscunha@gmail.com>
2023-06-17 02:28:38 -0300

    first commit

---
+++ src/main/java/breno/Fadesp/Challenge/ErrorHandler/ErrorHandler.java
@@ -0,0 +1,26 @@
+package breno.Fadesp.Challenge.ErrorHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+	protected ResponseEntity<Object> handleMethodArgumentNotValid( MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ErrorModel error = new ErrorModel(HttpStatus.BAD_REQUEST, "Validation Error", ex.getAllErrors().toString());
+		return new ResponseEntity<>(error.getDetails(), HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	private ResponseEntity<ErrorModel> handleEntityNotFound( EntityNotFoundException ex){
+		ErrorModel error = new ErrorModel(HttpStatus.NOT_FOUND, "Entity not found", ex.getMessage());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+}
